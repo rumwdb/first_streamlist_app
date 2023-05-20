fa50c81d711300bab35baed2bb7713cb640bdce0
rumwdb <134134651+rumwdb@users.noreply.github.com>
2023-05-20 15:58:36 +0530

    Update streamlist_app.py

--- streamlist_app.py
+++ streamlist_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title("My Parents New HEalthy Dinner")
+streamlist.header('Breakfast Menu')
+streamlist.text('Omega 3 & Blueberry oatmeal')
+streamlist.text('Kale, spinach and Rocket Smoothie')
+streamlist.text('Hard-Boiled Free-Range Egg')
