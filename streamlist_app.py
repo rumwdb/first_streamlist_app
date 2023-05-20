a26cb5046d2199ceb492b4209cfb099ad99b095d
rumwdb <134134651+rumwdb@users.noreply.github.com>
2023-05-20 16:03:50 +0530

    Update streamlist_app.py

--- streamlist_app.py
+++ streamlist_app.py
@@ -5,3 +5,6 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry oatmeal')
 streamlit.text('Kale, spinach and Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Avocado Toast')
+
+streamlit.header('Build your own friut smoothie')
