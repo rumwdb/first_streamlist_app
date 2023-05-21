72c8f923a4f336ee82f7120ddee1e499fa3a9c8c
rumwdb <134134651+rumwdb@users.noreply.github.com>
2023-05-21 13:03:34 +0530

    Update streamlist_app.py

--- streamlist_app.py
+++ streamlist_app.py
@@ -21,8 +21,8 @@ streamlit.header('Fruityvice Fruit advice')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-streamlit.text(fruityvice_response)
+#streamlit.text(fruityvice_response)
 
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
