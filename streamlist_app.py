ff896526c0134edefdeb5bdb0e2f1bbfbb53f889
rumwdb <134134651+rumwdb@users.noreply.github.com>
2023-05-21 13:02:20 +0530

    Update streamlist_app.py

--- streamlist_app.py
+++ streamlist_app.py
@@ -20,5 +20,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit advice')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 streamlit.text(fruityvice_response)
+
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
+
