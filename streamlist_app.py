bf3b3977650362b1e936c9442ea1d1bacf8bf7dd
rumwdb <134134651+rumwdb@users.noreply.github.com>
2023-05-22 07:20:26 +0530

    Update streamlist_app.py

--- streamlist_app.py
+++ streamlist_app.py
@@ -37,6 +37,6 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("the fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-add_my_fruit = streamlit.text_input('What fruit would you like add?")
+add_my_fruit = streamlit.text_input('What fruit would you like add?')
 streamlit.write('thanks for adding',add_my_fruit)                                 
                                     
