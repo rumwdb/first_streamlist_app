0163fee4f204daf56b5effad28d47f25c48c1b8e
rumwdb <134134651+rumwdb@users.noreply.github.com>
2023-05-22 07:44:21 +0530

    Update streamlist_app.py

--- streamlist_app.py
+++ streamlist_app.py
@@ -25,15 +25,20 @@ streamlit.header('Fruityvice Fruit advice')
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+    
 streamlit.header('Fruityvice Fruit advice!')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_funciton = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_funciton)
+
 
 except URLError as e:
   streamlit.error()
@@ -41,6 +46,7 @@ except URLError as e:
 
 #
 
+    
 #streamlit.text(fruityvice_response)
 
 
